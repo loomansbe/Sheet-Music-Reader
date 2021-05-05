a0c92eb409d91fdca3e3914cc06061efc1136d1e
loomansbe <loomansbe@uwplatt.edu>
2021-05-05 14:54:01 -0500

    Initial Demo project commit

---
+++ Sheet Music Assistant/Debug/sources.mk
@@ -0,0 +1,66 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+CC_SRCS := 
+C_SRCS := 
+CPP_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+OBJDUMP_LIST := 
+C_UPPER_DEPS := 
+S_DEPS := 
+C_DEPS := 
+OBJCOPY_BIN := 
+CC_DEPS := 
+SIZE_OUTPUT := 
+C++_DEPS := 
+EXECUTABLES := 
+OBJS := 
+CXX_DEPS := 
+S_UPPER_DEPS := 
+CPP_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Core/Src \
+Core/Startup \
+Drivers/BSP/Components/cs43l22 \
+Drivers/BSP/Components/exc7200 \
+Drivers/BSP/Components/ft6x06 \
+Drivers/BSP/Components/ili9325 \
+Drivers/BSP/Components/ili9341 \
+Drivers/BSP/Components/l3gd20 \
+Drivers/BSP/Components/lis302dl \
+Drivers/BSP/Components/lis3dsh \
+Drivers/BSP/Components/ls016b8uy \
+Drivers/BSP/Components/lsm303dlhc \
+Drivers/BSP/Components/mfxstm32l152 \
+Drivers/BSP/Components/otm8009a \
+Drivers/BSP/Components/ov2640 \
+Drivers/BSP/Components/ov5640 \
+Drivers/BSP/Components/s5k5cag \
+Drivers/BSP/Components/st7735 \
+Drivers/BSP/Components/st7789h2 \
+Drivers/BSP/Components/stmpe1600 \
+Drivers/BSP/Components/stmpe811 \
+Drivers/BSP/Components/ts3510 \
+Drivers/BSP/Components/wm8994 \
+Drivers/BSP/STM32F413H-Discovery \
+Drivers/STM32F4xx_HAL_Driver/Src \
+Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src \
+Middlewares/ST/STM32_USB_Host_Library/Core/Src \
+Middlewares/Third_Party/FatFs/src \
+Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 \
+Middlewares/Third_Party/FreeRTOS/Source \
+Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F \
+Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang \
+USB_HOST/App \
+USB_HOST/Target \
+
